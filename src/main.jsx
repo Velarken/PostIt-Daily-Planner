0a6a575e965112295e4b8d7a4a1600b5e3f8fa2f
Dillon Hailey <181585418+Velarken@users.noreply.github.com>
2026-03-21 21:14:03 -0400

    init project

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router'
+import { RouterProvider } from 'react-router'
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <App />,
-  },
-  {
-    path: '/',
-    element: ''
+    path:'/',
+    element: <App />
   }
 ])
 
